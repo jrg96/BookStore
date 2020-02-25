b427130046fb7bcd89c713d1c1fb5fe742dbeeed
jrg96 <gomezlopez.jorge96@gmail.com>
2020-02-25 16:01:15 -0600

    Basic Design for Product Upsert, Implemented ProductViewModel for populating Upsert

--- BookStore/Areas/Admin/Controllers/ProductController.cs
+++ BookStore/Areas/Admin/Controllers/ProductController.cs
@@ -2,16 +2,86 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookStoreDataAccess.Repository.IRepository;
+using BookStoreModels;
+using BookStoreModels.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BookStore.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class ProductController : Controller
     {
+        private readonly IProductRepository _productRepository;
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly ICoverTypeRepository _coverTypeRepository;
+
+        public ProductController(IProductRepository productRepository, 
+            ICategoryRepository categoryRepository,
+            ICoverTypeRepository coverTypeRepository)
+        {
+            _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
+            _coverTypeRepository = coverTypeRepository;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Upsert(int? id)
+        {
+            Product product = new Product();
+
+            if (id != null)
+            {
+                product = await this._productRepository.GetProduct(id.GetValueOrDefault());
+
+                if (product == null)
+                {
+                    return NotFound();
+                }
+            }
+
+            // Creating ViewModel for specific page
+            ProductViewModel productViewModel = new ProductViewModel();
+            productViewModel.Product = product;
+            productViewModel.CategoryList = (await _categoryRepository.GetCategories())
+                .Select(i => new SelectListItem { 
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                });
+            productViewModel.CoverTypeList = (await _coverTypeRepository.GetCoverTypes())
+                .Select(i => new SelectListItem {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                });
+
+            return View(productViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                if (category.Id == 0)
+                {
+                    _categoryRepository.Add<Category>(category);
+                }
+                else
+                {
+                    _categoryRepository.Update<Category>(category);
+                }
+
+                await _categoryRepository.SaveAll();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(category);
         }
     }
 }
--- BookStore/wwwroot/js/Product.js
+++ BookStore/wwwroot/js/Product.js
@@ -25,7 +25,7 @@ function loadDataTable() {
                 "render": function (data) {
                     return `
                             <div class="text-center">
-                                <a href="/Admin/CoverType/Upsert/${data}" class="btn btn-success text-white" style="cursor: pointer">
+                                <a href="/Admin/Product/Upsert/${data}" class="btn btn-success text-white" style="cursor: pointer">
                                     <i class="fa fa-edit"></i>
                                 </a>
                                 <a onclick=Delete("/api/covertypes/${data}") class="btn btn-danger text-white" style="cursor: pointer">
@@ -39,4 +39,28 @@ function loadDataTable() {
         ]
     });
 
+}
+
+function Delete(url) {
+    swal({
+        title: "Are you sure you want to delete?",
+        text: "You will not be able to restore the data",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    toastr.success("Category deleted successfully");
+                    dataTable.ajax.reload();
+                },
+                error: function (data) {
+                    toastr.error("Could not delete category: " + data);
+                }
+            });
+        }
+    });
 }
