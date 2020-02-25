b15c752570b2c48425ef674b88f3d3fdc97e0fbf
jrg96 <gomezlopez.jorge96@gmail.com>
2020-02-25 12:05:03 -0600

    ProductEntity Added

---
+++ BookStoreModels/Product.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace BookStoreModels
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string ISBN { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        [Required]
+        [Range(1, 10000)]
+        public double ListPrice { get; set; }
+
+        [Required]
+        [Range(1, 10000)]
+        public double Price { get; set; }
+
+        [Required]
+        [Range(1, 10000)]
+        public double Price50 { get; set; }
+
+        [Required]
+        [Range(1, 10000)]
+        public double Price100 { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public int CoverTypeId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+
+        [ForeignKey("CoverTypeId")]
+        public CoverType CoverType { get; set; }
+    }
+}
