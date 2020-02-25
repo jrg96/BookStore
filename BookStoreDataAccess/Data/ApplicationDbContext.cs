cca54eda0ee26bbb114a8c7c77bf7244474399e3
jrg96 <gomezlopez.jorge96@gmail.com>
2020-02-24 20:20:47 -0600

    Added Category Entity

--- BookStoreDataAccess/Data/ApplicationDbContext.cs
+++ BookStoreDataAccess/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using BookStoreModels;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,5 +13,7 @@ namespace BookStore.DataAccess.Data
             : base(options)
         {
         }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
