0f56cab1106326206ee37d7e14ff7f6288149860
jrg96 <gomezlopez.jorge96@gmail.com>
2020-02-24 22:29:57 -0600

    Display DataTable for Category List

---
+++ BookStore/wwwroot/js/Category.js
@@ -0,0 +1,34 @@
+﻿var dataTable;
+
+$(document).ready(function() {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTabe = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/api/categories",
+            "dataSrc": ""
+        },
+        "columns": [
+            { "data": "name", "width": "60%" },
+            {
+                "data": "id", 
+                "render": function (data) {
+                    return `
+                            <div class="text-center">
+                                <a href="/Admin/Category/Upsert/${data}" class="btn btn-success text-white" style="cursor: pointer">
+                                    <i class="fa fa-edit"></i>
+                                </a>
+                                <a class="btn btn-danger text-white" style="cursor: pointer">
+                                    <i class="fa fa-trash"></i>
+                                </a>
+                            </div>
+                           `;
+                },
+                "width": "40%"
+            }
+        ]
+    });
+
+}
