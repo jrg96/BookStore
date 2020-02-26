8616148dc972612455f2a4bd3a18ead3025f4098
jrg96 <gomezlopez.jorge96@gmail.com>
2020-02-25 22:15:42 -0600

    Company CRUD implemented

---
+++ BookStore/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStoreDataAccess.Repository.IRepository;
+using BookStoreModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CompanyController : Controller
+    {
+        private readonly ICompanyRepository _companyRepository;
+
+        public CompanyController(ICompanyRepository companyRepository)
+        {
+            _companyRepository = companyRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Upsert(int? id)
+        {
+            Company company = new Company();
+
+            if (id == null)
+            {
+                return View(company);
+            }
+
+            company = await this._companyRepository.GetCompany(id.GetValueOrDefault());
+
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            return View(company);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(Company company)
+        {
+            if (ModelState.IsValid)
+            {
+                if (company.Id == 0)
+                {
+                    _companyRepository.Add<Company>(company);
+                }
+                else
+                {
+                    _companyRepository.Update<Company>(company);
+                }
+
+                await _companyRepository.SaveAll();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(company);
+        }
+    }
+}
