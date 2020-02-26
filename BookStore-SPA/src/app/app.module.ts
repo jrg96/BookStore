995a9624426b039aff47b12ec50b08cd38ad21e5
jrg96 <gomezlopez.jorge96@gmail.com>
2020-02-26 16:58:18 -0600

    Installed ngx-bootstrap

--- BookStore-SPA/src/app/app.module.ts
+++ BookStore-SPA/src/app/app.module.ts
@@ -2,15 +2,49 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+
+// Habilitar Angular routing
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
+
+// Formularios y Cliente Http
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+// Evitar errores con NGX bootstrap
+import { HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+export class CustomHammerConfig extends HammerGestureConfig  {
+  overrides = {
+      pinch: { enable: false },
+      rotate: { enable: false }
+  };
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      NavComponent
+   ],
+   imports: [
+      BrowserModule,
+      
+      // Evitar errores del bootstrap
+      BrowserAnimationsModule,
+
+      // Routing, Http y formularios
+      FormsModule,
+      RouterModule.forRoot(appRoutes),
+      HttpClientModule
+   ],
+   providers: [
+      // Evitar errores del bootstrap
+      { provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig }
+   ],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
